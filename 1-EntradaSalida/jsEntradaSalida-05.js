65628fe0ba722ac2a335ddc8ac38d1ddf0227fc3
Giovanni <giolucarna@gmail.com>
2022-01-22 01:44:25 -0300

    Feat :tada: upgraded and fixed syntaxis!

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,8 +1,30 @@
 /*
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
+
+alert ("Enter your first name please & then enter your age please!");
+
 function mostrar()
 {	
-	alert("ok");
+	var nameAsk;
+	var ageAsk;
+	var concatenatedNameAndAge;
+
+	nameAsk = document.getElementById("txtIdNombre").value;
+
+	ageAsk = document.getElementById("txtIdEdad").value; 
+
+	if(isNaN(nameAsk) && !isNaN(ageAsk))
+	{
+		alert("All the asked data was validated and now it'll be desplayed together");
+
+		concatenatedNameAndAge = "Usted se llama ";
+		concatenatedNameAndAge += nameAsk;
+		concatenatedNameAndAge += " y tiene ";
+		concatenatedNameAndAge += ageAsk;
+		concatenatedNameAndAge += " años";
+
+		alert(concatenatedNameAndAge);
+	}
 }
 
