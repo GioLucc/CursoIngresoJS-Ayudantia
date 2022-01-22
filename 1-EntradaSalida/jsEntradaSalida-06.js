62b086102aa86d109b476029e826d8643434b87b
Giovanni <giolucarna@gmail.com>
2022-01-22 01:43:15 -0300

    Feat :tada: Solved and enhanced multiple exercises!

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,25 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var firstNumber;
+	var secondNumber;
+	var sumatoryOfNumbers;
+
+	firstNumber = document.getElementById("txtIdNumeroUno").value;
+	secondNumber = document.getElementById("txtIdNumeroDos").value;
+
+	if(!isNaN(firstNumber) && !isNaN(secondNumber))
+	{
+		alert("Thanks for putting two numbers!");
+		firstNumber = parseInt(firstNumber);
+		secondNumber = parseInt(secondNumber);
+		sumatoryOfNumbers = (firstNumber + secondNumber);
+
+		alert("The sumatory of the numbers are "+ sumatoryOfNumbers);
+	}
+	else
+	{
+		alert("Try putting your numbers again!");
+	}
 }
 
--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -3,23 +3,110 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
+alert("Welcome to the Admission Course Calculator");
+alert("Please insert two numbers in the proper spaces");
+alert("After inserting numbers you can do whatever operation you would like!");
 function sumar()
-{	
-	alert("ok");	
+{
+	var firstNumber;
+	var secondNumber;
+	var concatenatedOperation;
+
+	firstNumber = document.getElementById("txtIdNumeroUno").value;
+	secondNumber = document.getElementById("txtIdNumeroDos").value;
+
+	if(!isNaN(firstNumber) && !isNaN (secondNumber))
+	{
+		alert("Thanks for placing the asked numbers correctly!")
+
+		firstNumber = parseInt(firstNumber);
+		secondNumber = parseInt(secondNumber);
+
+		concatenatedOperation = 'The sumatory of the numbers added earlier is = ';
+		concatenatedOperation += (firstNumber + secondNumber);
+		alert(concatenatedOperation);
+	}
+	else
+	{
+		alert("One of the two asked numbers wasn't a number, please check it and retry!")
+	}
 }
 
 function restar()
 {
-	
+	var firstNumber;
+	var secondNumber;
+	var concatenatedOperation;
+
+	firstNumber = document.getElementById("txtIdNumeroUno").value;
+	secondNumber = document.getElementById("txtIdNumeroDos").value;
+
+	if(!isNaN(firstNumber) && !isNaN (secondNumber))
+	{
+		alert("Thanks for placing the asked numbers correctly!")
+
+		firstNumber = parseInt(firstNumber);
+		secondNumber = parseInt(secondNumber);
+
+		concatenatedOperation = 'The rest of the numbers added earlier is = ';
+		concatenatedOperation += (firstNumber - secondNumber);
+		alert(concatenatedOperation);
+	}
+	else
+	{
+		alert("One of the two asked numbers wasn't a number, please check it and retry!")
+	}
 }
 
 function multiplicar()
 { 
-	
+	var firstNumber;
+	var secondNumber;
+	var concatenatedOperation;
+
+	firstNumber = document.getElementById("txtIdNumeroUno").value;
+	secondNumber = document.getElementById("txtIdNumeroDos").value;
+
+	if(!isNaN(firstNumber) && !isNaN (secondNumber))
+	{
+		alert("Thanks for placing the asked numbers correctly!")
+
+		firstNumber = parseInt(firstNumber);
+		secondNumber = parseInt(secondNumber);
+
+		concatenatedOperation = 'The multiplication of the numbers added earlier is = ';
+		concatenatedOperation += (firstNumber * secondNumber);
+		alert(concatenatedOperation);
+	}
+	else
+	{
+		alert("One of the two asked numbers wasn't a number, please check it and retry!")
+	}
 }
 
 function dividir()
 {
-	
-}
+	var firstNumber;
+	var secondNumber;
+	var concatenatedOperation;
 
+	firstNumber = document.getElementById("txtIdNumeroUno").value;
+	secondNumber = document.getElementById("txtIdNumeroDos").value;
+
+	if(!isNaN(firstNumber) && !isNaN (secondNumber))
+	{
+		alert("Thanks for placing the asked numbers correctly!")
+
+		firstNumber = parseInt(firstNumber);
+		secondNumber = parseInt(secondNumber);
+
+		concatenatedOperation = 'The division of the numbers added earlier is = ';
+		concatenatedOperation += (firstNumber / secondNumber);
+		alert(concatenatedOperation);
+	}
+	else
+	{
+		alert("One of the two asked numbers wasn't a number, please check it and retry!")
+	}
+}
