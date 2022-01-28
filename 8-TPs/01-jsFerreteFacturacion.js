86aeedefe3b783f955225bb30bf9aeeaad4ec054
Giovanni <giolucarna@gmail.com>
2022-01-27 23:24:40 -0300

    Feat :tada: Solved all the exercises and added validations!

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -3,15 +3,98 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
+alert("Welcome to 'Ferrete Facturación' Please enter the price of each product and then hit a button to realize the operation");
+
 function Sumar () 
 {
-	
-}
+    var enterFirstPrice;
+    var enterSecondPrice;
+    var enterThirdPrice;
+    var pricesSumatory;
+
+    enterFirstPrice = document.getElementById("txtIdPrecioUno").value;
+    enterSecondPrice = document.getElementById("txtIdPrecioDos").value;
+    enterThirdPrice = document.getElementById("txtIdPrecioTres").value;
+
+    if(!isNaN(enterFirstPrice) && !isNaN(enterSecondPrice) && !isNaN (enterThirdPrice))
+    {
+        alert("Thanks for entering all the prices correctly!");
+
+        enterFirstPrice = parseInt(enterFirstPrice);
+        enterSecondPrice = parseInt(enterSecondPrice);
+        enterThirdPrice = parseInt(enterThirdPrice);
+
+        pricesSumatory = enterFirstPrice + enterSecondPrice + enterThirdPrice;
+
+        alert("The sumatory of all prices is $"+pricesSumatory+"!");
+    }
+    else
+    {
+        alert("You've entered something different than a number in one of the three boxes! Try again");
+    }
+    
+}    
 function Promedio () 
 {
-	
+	var enterFirstPrice;
+    var enterSecondPrice;
+    var enterThirdPrice;
+    var pricesSumatory;
+    var pricesAverage;
+
+    enterFirstPrice = document.getElementById("txtIdPrecioUno").value;
+    enterSecondPrice = document.getElementById("txtIdPrecioDos").value;
+    enterThirdPrice = document.getElementById("txtIdPrecioTres").value;
+
+    if(!isNaN(enterFirstPrice) && !isNaN(enterSecondPrice) && !isNaN (enterThirdPrice))
+    {
+        alert("Thanks for entering all the prices correctly!");
+
+        enterFirstPrice = parseInt(enterFirstPrice);
+        enterSecondPrice = parseInt(enterSecondPrice);
+        enterThirdPrice = parseInt(enterThirdPrice);
+
+        pricesSumatory = enterFirstPrice + enterSecondPrice + enterThirdPrice;
+        pricesAverage = pricesSumatory / 3;
+        pricesAverage = pricesAverage.toFixed(2); // 2.d;
+
+        alert("The sumatory of all prices is $"+pricesSumatory+"! and it's average is $"+pricesAverage+"!");
+    }
+    else
+    {
+        alert("You've entered something different than a number in one of the three boxes! Try again");
+    }
 }
 function PrecioFinal () 
 {
-	
+    var enterFirstPrice;
+    var enterSecondPrice;
+    var enterThirdPrice;
+    var pricesSumatory;
+    var IvaPrice;
+    var finalPricePlus21Iva;
+
+    enterFirstPrice = document.getElementById("txtIdPrecioUno").value;
+    enterSecondPrice = document.getElementById("txtIdPrecioDos").value;
+    enterThirdPrice = document.getElementById("txtIdPrecioTres").value;
+
+    if(!isNaN(enterFirstPrice) && !isNaN(enterSecondPrice) && !isNaN (enterThirdPrice))
+    {
+        alert("Thanks for entering all the prices correctly!");
+
+        enterFirstPrice = parseInt(enterFirstPrice);
+        enterSecondPrice = parseInt(enterSecondPrice);
+        enterThirdPrice = parseInt(enterThirdPrice);
+
+        pricesSumatory = enterFirstPrice + enterSecondPrice + enterThirdPrice;
+        IvaPrice = pricesSumatory * 0.21;
+        finalPricePlus21Iva = pricesSumatory + IvaPrice;
+
+        alert("The sumatory of all prices is $"+pricesSumatory+"! your final price with the 21% of IVA is $"+finalPricePlus21Iva+"!");
+    }
+    else
+    {
+        alert("You've entered something different than a number in one of the three boxes! Try again");
+    }
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,18 +1,91 @@
 /*2.	Para el departamento de Construcción:
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
-B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
+B.	mostrar la cantidad de alambre a comprar si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
 function Rectangulo () 
 {
+    var enterLenght;
+    var enterWidth;
+    var landPerimeter;
+    var wireQtyToBuy;
 
+    enterLenght = document.getElementById("txtIdLargo").value;
+    enterWidth = document.getElementById("txtIdAncho").value;
+
+    if(!isNaN(enterLenght) && !isNaN(enterWidth))
+    {
+        alert("Thanks for entering correctly the Lenght and Width of your land!");
+
+        enterLenght = parseInt(enterLenght);
+        enterWidth = parseInt(enterWidth);
+
+        landPerimeter = (enterLenght * 2) + (enterWidth *2);
+
+        wireQtyToBuy = landPerimeter * 3;
+
+        alert("The quantity of wire you have to buy according to the perimeter of your land is ("+wireQtyToBuy+")Meters!");
+    }
+    else
+    {
+        alert("You've entered something different than a number in the Lenght or Width boxes! Try again");
+    }
 }
 function Circulo () 
 {
+    var enterRadius;
+    var radiusOperation;
+    var wireQtyToBuy;
+
+    enterRadius = document.getElementById("txtIdRadio").value;
+
+    if(!isNaN(enterRadius))
+    {
+        alert("Thanks for entering correctly the Radius of your land!");
+
+        enterRadius = parseInt(enterRadius);
+
+        radiusOperation = 2 * Math.PI * enterRadius;
+
+        wireQtyToBuy = radiusOperation * 3;
+        wireQtyToBuy = wireQtyToBuy.toFixed(2);
+
+        alert("The quantity of wire you have to buy according to the radius of your land is ("+wireQtyToBuy+")Meters!");
+    }
+    else
+    {
+        alert("You've entered something different than a number in the Radius Box! Try again");
+    }
 	
 }
 function Materiales () 
 {
-	
+    var enterLenght;
+    var enterWidth;
+    var landPerimeter;
+    var limeToBuy;
+    var cementToBuy;
+
+    enterLenght = document.getElementById("txtIdLargo").value;
+    enterWidth = document.getElementById("txtIdAncho").value;
+
+    if(!isNaN(enterLenght) && !isNaN(enterWidth))
+    {
+        alert("Thanks for entering correctly the Lenght and Width of your land!");
+
+        enterLenght = parseInt(enterLenght);
+        enterWidth = parseInt(enterWidth);
+
+        landPerimeter = enterLenght * enterWidth;
+
+        cementToBuy = enterLenght * 2;
+        limeToBuy = enterWidth * 3;
+
+        alert("The quantity of cement and lime you have to buy according to the perimeter of your land is ("+cementToBuy+") cement and ("+limeToBuy+") lime!");
+    }
+    else
+    {
+        alert("You've entered something different than a number in the Lenght or Width boxes! Try again");
+    }
 }
